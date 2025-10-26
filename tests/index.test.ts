347024295a724ff39836f585161264d72c400f3b
Christian Nuss <christian.nuss@gmail.com>
2025-10-26 09:04:33 -0400

    more test scaffolding

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,15 @@
-import { id } from '@scaffoldly/rowdy-grpc';
+import { id, NAME, VERSION } from '@scaffoldly/rowdy-grpc';
 import packageJson from '../package.json';
 
 describe('Library Tests', () => {
+  it('should output the correct name', () => {
+    expect(NAME).toBe(packageJson.name);
+  });
+
+  it('should output the correct version', () => {
+    expect(VERSION).toBe(packageJson.version);
+  });
+
   it('should output the correct id', () => {
     expect(id()).toBe(`${packageJson.name}@${packageJson.version}`);
   });
