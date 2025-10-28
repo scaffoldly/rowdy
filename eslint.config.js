2f36ae975dbf9f0640cb20547605992f2e2755d1
Christian Nuss <christian.nuss@gmail.com>
2025-10-28 05:20:54 -0400

    fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -5,92 +5,53 @@ const prettier = require('eslint-plugin-prettier');
 const prettierConfig = require('eslint-config-prettier');
 const globals = require('globals');
 
+// Common TypeScript rules
+const commonTsRules = {
+  ...typescript.configs.recommended.rules,
+  ...prettierConfig.rules,
+  '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+  '@typescript-eslint/explicit-function-return-type': 'warn',
+  '@typescript-eslint/no-explicit-any': 'error',
+  '@typescript-eslint/no-require-imports': 'off',
+  'no-case-declarations': 'off',
+  'prettier/prettier': 'error',
+};
+
+// Common TypeScript config
+const createTsConfig = (files, project, extraGlobals = {}, ruleOverrides = {}) => ({
+  files,
+  languageOptions: {
+    parser: typescriptParser,
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      project,
+    },
+    globals: {
+      ...globals.node,
+      ...extraGlobals,
+    },
+  },
+  plugins: {
+    '@typescript-eslint': typescript,
+    prettier,
+  },
+  rules: {
+    ...commonTsRules,
+    ...ruleOverrides,
+  },
+});
+
 module.exports = [
-  // Global ignores
-  {
-    ignores: ['node_modules/**', 'dist/**', '**/*.d.ts'],
-  },
-
-  // ESLint config file itself
-  {
-    files: ['eslint.config.js'],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
-  },
-
-  // Base JavaScript config
+  { ignores: ['node_modules/**', 'dist/**', '**/*.d.ts'] },
+  { files: ['eslint.config.js'], languageOptions: { globals: globals.node } },
   js.configs.recommended,
-
-  // TypeScript files (excluding tests)
+  createTsConfig(['src/**/*.ts'], './tsconfig.json', globals.browser, { 'no-console': 'warn' }),
   {
-    files: ['src/**/*.ts'],
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        project: './tsconfig.json',
-      },
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescript,
-      prettier,
-    },
-    rules: {
-      ...typescript.configs.recommended.rules,
-      ...prettierConfig.rules,
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-require-imports': 'off',
-      'no-console': 'warn',
-      'no-case-declarations': 'off',
-      'prettier/prettier': 'error',
-    },
+    files: ['tests/**/*.js'],
+    languageOptions: { ecmaVersion: 2020, sourceType: 'commonjs', globals: globals.node },
+    plugins: { prettier },
+    rules: { ...prettierConfig.rules, 'prettier/prettier': 'error' },
   },
-
-  // Test files
-  {
-    files: ['tests/**/*.ts'],
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        project: './tests/tsconfig.json',
-      },
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescript,
-      prettier,
-    },
-    rules: {
-      ...typescript.configs.recommended.rules,
-      ...prettierConfig.rules,
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-require-imports': 'off',
-      'no-console': 'off',
-      'no-case-declarations': 'off',
-      'prettier/prettier': 'error',
-    },
-  },
+  createTsConfig(['tests/**/*.ts'], './tests/tsconfig.json', globals.jest, { 'no-console': 'off' }),
 ];
