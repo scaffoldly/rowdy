7675605f88a6e08c0e30676c801d4a043d59ba7d
Christian Nuss <christian.nuss@gmail.com>
2025-11-17 08:25:02 -0500

    switch snapshot to ReadCommandOutput

--- src/cdk.ts
+++ src/cdk.ts
@@ -56,7 +56,7 @@ export class CloudResource<Resource, ReadCommandOutput> implements PromiseLike<P
   private options: ResourceOptions = {};
   private desired?: Partial<ReadCommandOutput>;
   private _tags: Record<string, string> = {};
-  private _snapshot?: Partial<Resource>;
+  private _snapshot?: Partial<ReadCommandOutput>;
 
   constructor(
     public readonly requests: {
@@ -73,13 +73,11 @@ export class CloudResource<Resource, ReadCommandOutput> implements PromiseLike<P
     this._tags = tags;
   }
 
-  get Snapshot(): PromiseLike<Partial<Resource>> {
+  get Snapshot(): PromiseLike<Partial<ReadCommandOutput>> {
     if (this._snapshot) {
       return Promise.resolve(this._snapshot);
     }
-    return this.manage()
-      .then((resource) => (this._snapshot = resource))
-      .then(() => this.Snapshot);
+    return this.manage().then(() => this.Snapshot);
   }
 
   get Tags(): Record<string, string> {
@@ -162,6 +160,7 @@ export class CloudResource<Resource, ReadCommandOutput> implements PromiseLike<P
           }
 
           const resource = this.resourceExtractor(readResponse);
+          this._snapshot = readResponse;
 
           return resource;
         } catch (e) {
