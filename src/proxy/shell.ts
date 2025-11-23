ca4768a4621841b6c40815f8ebfb5ed7caaace29
Christian Nuss <christian.nuss@gmail.com>
2025-11-22 23:26:01 -0500

    cleanup imports

--- src/proxy/shell.ts
+++ src/proxy/shell.ts
@@ -4,7 +4,6 @@ import { PassThrough, Writable } from 'stream';
 import { ILoggable, log, Logger, Trace } from '../log';
 import { execa } from 'execa';
 import type { Options } from 'execa';
-import { Environment } from '..';
 
 export class ShellRequest<P extends Pipeline> extends Request<P> {
   constructor(
